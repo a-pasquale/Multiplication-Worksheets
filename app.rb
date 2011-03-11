1ac6c6540d26f06704caa6b5e7e902f8e06a5168
Andrew Pasquale <andrew@elytra.net>
2011-03-11 15:40:13 -0500

    Added form for index page allowing you to select number of problems and which multiplication tables you wish to use.

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
+  haml :index
+end
+
+post '/multiply' do
+  num_probs = params[:num_probs]
+  @num_rows = num_probs.to_i / 5
+  @nums = params[:nums]
   haml :multiply
+
 end
 
