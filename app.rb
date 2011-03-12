94716c26ee82baf31104d7c7a07246963d00bfa8
Andrew Pasquale <andrew@elytra.net>
2011-03-11 22:02:14 -0500

    Added puzzle.

--- app.rb
+++ app.rb
@@ -6,11 +6,39 @@ get '/' do
   haml :index
 end
 
+post '/puzzle' do
+  @num_probs = 12
+  lnums = params[:lnums]
+  rnums = params[:rnums]
+  
+  @a = Array.new
+  @b = Array.new
+  @c = Array.new
+  @d = Array.new
+
+  @num_probs.times do
+    @a << lnums[rand(lnums.size)]
+    @b << rnums[rand(rnums.size)]
+    @c << lnums[rand(lnums.size)]
+    @d << rnums[rand(rnums.size)]
+  end
+
+  haml :puzzle
+end
+
+
 post '/multiply' do
   num_probs = params[:num_probs]
   @num_rows = num_probs.to_i / 5
-  @nums = params[:nums]
-  haml :multiply
+  @lnums = params[:lnums]
+  @rnums = params[:rnums]
+
+  # do some basic sanity checking
+  if !@lnums or !@rnums or num_probs.to_i > 200 then
+    redirect '/'
+  else
+    haml :multiply
+  end
 
 end
 
